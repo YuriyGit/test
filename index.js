cc3202c27aa52ce141c189aee1496d70499227a5
YuriyWeb <yuriy.mayorov.web@mail.ru>
2023-01-31 20:58:06 +0400

    first commit

---
+++ index.js
@@ -0,0 +1,66 @@
+const express = require('express')
+const path = require('path')
+const fs = require('fs')
+const {v4: uuid} = require('uuid')
+const PORT = process.env.PORT || 3000
+const app = express()
+
+class Book {
+    constructor(id = uuid(), title = 'string', authors = 'string', favorite = 'string', fileCover = 'string', fileName = 'string') {
+
+        {
+            this.id = id
+            this.title = title
+            this.authors = authors
+            this.favorite = favorite
+            this.fileCover = fileCover
+            this.fileName = fileName
+        }
+    }
+}
+
+async function getBookList() {
+    await new Promise((resolve, reject) => {
+        fs.readdir(path.join(__dirname, 'books'), (err, books) => {
+            if (err) reject(err)
+            console.log("books: ", books)//         !!!!!!!!!!
+            resolve(books)//                        !!!!!!!!!!
+            return books//                          !!!!!!!!!!
+        })
+    })
+}
+
+
+app.post('/api/user/login', (req, res) => {
+    res
+        .status(201)
+        .send({id: 1, mail: "test@mail.ru"})
+})
+
+app.get('/api/books', (req, res) => {
+
+    res.send(getBookList())
+})
+
+app.post('/api/books', (req, res) => {
+    // res.send('создаём книгу и возвращаем её же вместе с присвоенным ID')
+    const book = new Book()
+    res.send(`Book ${book}\n book id: ${book.id}`)
+})
+
+app.get('/api/books/:id', (req, res) => {
+    res.send('получить книгу по ID \n получаем объект книги, если запись не найдена, вернём Code: 404')
+
+})
+
+app.put('/api/books/:id', (req, res) => {
+    res.send('редактировать книгу по ID\n редактируем объект книги, если запись не найдена, вернём Code: 404')
+})
+
+app.delete('/api/books/:id', (req, res) => {
+    res.send('удалить книгу по ID \n удаляем книгу и возвращаем ответ: \'ok\'')
+})
+
+app.listen(PORT, (err) => {
+    err ? console.error(err) : console.log(`Server started on a port ${PORT}...`)
+})
